c3238c12549f8121ff792cac90b4625d36483a64
Arthur Siqueira <asesilva@inf.ufpel.edu.br>
2019-06-06 16:52:39 -0300

    Layout with sidebar

--- tela/src/components/Sidebar/Sidebar.js
+++ tela/src/components/Sidebar/Sidebar.js
@@ -4,21 +4,49 @@ import Icon from "../helpers/Icon";
 import { ICONS } from "../../global/constants";
 
 class Sidebar extends Component {
+  state = {
+    links: [
+      { icon: ICONS.BRIEFCASE, name: "Key Metrics" },
+      { icon: ICONS.STAR, name: "Key Metric" },
+      { icon: ICONS.STAR, name: "Key Metric" },
+      { icon: ICONS.STAR, name: "Key Metric" }
+    ],
+    selected: false
+  };
+
+  selectLink = i => {
+    this.setState({ selected: i });
+  };
+
   render() {
+    const { links, selected } = this.state;
+
     return (
       <div className={classes.Sidebar}>
-        <SidebarLink icon={ICONS.STAR} />
-        <SidebarLink icon={ICONS.STAR} />
-        <SidebarLink icon={ICONS.STAR} />
-        <SidebarLink icon={ICONS.STAR} />
+        {links.map((link, i) => {
+          return (
+            <SidebarLink
+              icon={link.icon}
+              name={link.name}
+              selected={selected === i}
+              onClick={this.selectLink.bind(this, i)}
+              key={i}
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
 const SidebarLink = props => {
+  let className = classes.SidebarLink;
+  if (props.selected) {
+    className = [className, classes.selected].join(" ");
+  }
+
   return (
-    <div className={classes.SidebarLink}>
+    <div className={className} onClick={props.onClick}>
       <Icon icon={props.icon} />
       <span>Key Metrics</span>
     </div>
